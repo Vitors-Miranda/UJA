8d0d77fbf042037a9b0c587f926d8087f70ccf98
Vitor Samuel Miranda de Souza <82851236+Vitors-Miranda@users.noreply.github.com>
2024-03-04 21:12:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,78 @@
 const login = document.getElementById("login"),
 sign = document.getElementById("sign"),
 sobre = document.getElementById("sobre"),
+blog = document.getElementById("blog"),
+add = document.getElementById("add"),
 btn_registro = document.getElementById("tab-registro"),
 btn_login = document.getElementById("tab-login"),
+btn_blog = document.getElementById("tab-blog"),
+btn_add = document.getElementById("tab-add"),
+login_form = document.getElementById("login-form"),
 btn_sobre = document.getElementById("tab-sobre")
 
+function Clear(){
+    const list = [sign, login, sobre, blog, add]
+
+    for(let element of list){
+        element.style = "display: none"
+    }
+}
+
 function Home() {
-    sign.style = "display: none"
+    Clear()
     login.style = "display: flex"
-    sobre.style = "display: none"
+    btn_blog.style = "display: none"
+    btn_add.style = "display: none"
 }
 function Registro() {
+    Clear()
     sign.style = "display: flex"
-    login.style = "display: none"
-    sobre.style = "display: none"
 }
 function Sobre() {
-    sign.style = "display: none"
-    login.style = "display: none"
+    Clear()
     sobre.style = "display: flex"
+}
+function Blog() {
+    Clear()
+    blog.style = "display: flex"
+}
+function Add() {
+    Clear()
+    add.style = "display: flex"
+}
+login_form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    
+    let datos = new FormData(e.target)
+    datos = {
+        user: datos.get("user"),
+        password: datos.get("password"),
+    };
+    fetch("http://labtelema.ujaen.es:8083/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(datos)
+      })
+    .then(data => {
+            if(data.status == 200){
+                btn_login.style = "display: none"
+                btn_registro.style = "display: none"
+                btn_blog.style = "display: block"
+                btn_add.style = "display: block"
+                Blog()
+                List(datos.user)
+            }
+    });
+})
+function List(usuario){      
+      fetch("http://labtelema.ujaen.es:8083/blog/" +usuario, {
+        method: "GET"
+      })
+        .then(data => {
+            console.log(data)
+        });
 }
 btn_registro.addEventListener("click", () =>{
     Registro()
@@ -28,5 +82,11 @@ btn_login.addEventListener("click", () =>{
 })
 btn_sobre.addEventListener("click", () =>{
     Sobre()
+})
+btn_blog.addEventListener("click", () =>{
+    Blog()
+})
+btn_add.addEventListener("click", () =>{
+    Add()
 })
 Home()
